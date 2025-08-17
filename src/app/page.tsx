bd28dd7e310142cf8cde098f5c38cf121ae1d1c7
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-08-17 15:36:37 -0400

    Move `SecondHand` to its own file to avoid `"use client"` conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React from "react";
-import { connection } from 'next/server'
 import { useEffect, useState } from "react";
+import { SecondHand } from "./SecondHand";
 
 export default function Page() {
   return (
@@ -52,23 +52,4 @@ function Clock() {
       </svg>
     </div>
   );
-}
-
-export async function SecondHand({ secondRotation }: { secondRotation: number }) {
-  await connection();
-  const time = new Date();
-  const secondRotationFallback = time.getSeconds() * 6 + time.getMilliseconds() * 0.006;
-
-  return (
-    <line
-      x1="50"
-      y1="50"
-      x2="50"
-      y2="10"
-      stroke="red"
-      strokeWidth="1"
-      transform={`rotate(${secondRotation ?? secondRotationFallback}, 50, 50)`}
-      style={{ transition: "transform 0.1s linear" }}
-    />
-  )
 }
