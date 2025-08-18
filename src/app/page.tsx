b61874cbeb033314656a642c4de0530fb9d77626
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-08-17 20:34:34 -0400

    Change heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <div className="flex flex-row items-center justify-center h-screen">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold">Broken</h1>
+        <h1 className="text-2xl font-bold">It… works?</h1>
         <Clock>
           <SecondHand secondRotation={secondRotation} />
         </Clock>
